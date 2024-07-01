512d0d689e66f41b8fc482002aa4a9d72f337e4a
amirshayanyian <wahtamir12@gmail.com>
2024-07-01 11:30:52 +0330

    added logic to signUp controller and service

--- src/modules/auth/user.controller.js
+++ src/modules/auth/user.controller.js
@@ -1,3 +1,4 @@
+const { hashPass } = require('../../common/utils/auth.util');
 const UserService = require('./user.service');
 
 class UserController {
@@ -6,11 +7,40 @@ class UserController {
     autoBind(this);
     this.#service = new UserService();
   }
-  signUp(req,res,next){
+  signUp(req, res, next) {
     try {
-        
+      const {
+        username,
+        password,
+        first_name,
+        last_name,
+        address,
+        email,
+        mobile,
+      } = req.body;
+      const user = this.#service.signUp({
+        username,
+        password: hashPass(password),
+        first_name,
+        last_name,
+        address,
+        email,
+        mobile,
+      });
+      return res.status(201).json({
+        status: 201,
+        message: 'Created',
+        data: user,
+      });
     } catch (error) {
-        next(error)
+      if (error.code === 11000) {
+        return res.send({
+          status: 400,
+          type: 'Bad Request',
+          message: 'duplicate username! try changing it ',
+        });
+      }
+      next(error);
     }
   }
 }
--- src/modules/auth/user.service.js
+++ src/modules/auth/user.service.js
@@ -6,6 +6,8 @@ class UserService {
     autoBind(this);
     this.#model = userModel;
   }
-  
+  signUp(userModel) {
+    return this.#model.create(userModel);
+  }
 }
 module.exports = UserService;
