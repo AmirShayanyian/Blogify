4e7e803ff0d75e008a3c8f852f651f61c4829d76
amirshayanyian <wahtamir12@gmail.com>
2024-07-01 09:52:21 +0330

    added not found handler middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectToMongoose = require('./src/configs/mongoose.config');
+const NotFoundHandler = require('./src/common/middlewares/notFound.handler');
 require('dotenv').config();
 const port = process.env.SERVER_PORT || 3000;
 const mongoDB_url = process.env.MONGODB_URL;
@@ -7,6 +8,8 @@ const mongoDB_url = process.env.MONGODB_URL;
 async function app() {
   const app = express();
   await connectToMongoose(mongoDB_url);
+
+  NotFoundHandler(app);
   app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
   });
