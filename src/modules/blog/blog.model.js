7b5aae2452e8af88a99a67ce984eb3a958d27561
amirshayanyian <wahtamir12@gmail.com>
2024-07-01 10:18:59 +0330

    made blog model

--- src/modules/blog/blog.model.js
+++ src/modules/blog/blog.model.js
@@ -1,6 +1,16 @@
-const { Schema, Mongoose, model } = require('mongoose');
+const { Schema, model, Mongoose, Types } = require('mongoose');
 
-const blogSchema = Schema({}, { timestamps: true });
+const blogSchema = Schema(
+  {
+    title: { type: String, required: true, index: true },
+    short_desc: { type: String, required: false },
+    long_desc: { type: String, required: true },
+    author: { type: Types.ObjectId, required: true },
+    likes: { type: [Types.ObjectId] },
+    pictures: { type: [String] },
+  },
+  { timestamps: true }
+);
 
 const blogModel = model('blog', blogSchema);
 
