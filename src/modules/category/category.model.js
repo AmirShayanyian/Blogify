95d310fbcfd3a03ac7da44a9c709b9281ddf6533
amirshayanyian <wahtamir12@gmail.com>
2024-07-01 10:27:39 +0330

    made category model

--- src/modules/category/category.model.js
+++ src/modules/category/category.model.js
@@ -1,6 +1,14 @@
-const { Schema, Mongoose, model } = require('mongoose');
+const { Schema, Mongoose, model, Types } = require('mongoose');
 
-const categorySchema = Schema({}, { timestamps: true });
+const categorySchema = Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    description: { type: String },
+    picture: { type: String },
+    blogs: { type: [Types.ObjectId] },
+  },
+  { timestamps: true }
+);
 
 const categoryModel = model('category', categorySchema);
 
