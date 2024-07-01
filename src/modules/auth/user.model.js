993347443bf5df12e6af4c404a1aa6f3fb3e8dee
amirshayanyian <wahtamir12@gmail.com>
2024-07-01 10:24:47 +0330

    made user model

--- src/modules/auth/user.model.js
+++ src/modules/auth/user.model.js
@@ -1,6 +1,20 @@
-const { Schema, Mongoose, model } = require('mongoose');
+const { Schema, Types } = require('mongoose');
 
-const userSchema = Schema({}, { timestamps: true });
+const userSchema = Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    first_name: { type: String },
+    last_name: { type: String },
+    email: { type: String, required: true, unique: true },
+    mobile: { type: String, required: true, unique: true },
+    address: { type: String },
+    bookmarks: { type: [Types.ObjectId] },
+    likes: { type: [Types.ObjectId] },
+    profile_pic: { type: String },
+  },
+  { timestamps: true }
+);
 
 const userModel = model('user', userSchema);
 
